61bcaec40df6a696c40c0d33be5fd02842af4d77
Gustavo Snts <gustavosnts.contato@gmail.com>
2023-07-15 01:05:14 -0300

    Turn the code into OOP architecture

--- Pyautocoin.py
+++ Pyautocoin.py
@@ -1,48 +1,65 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-availableCoin = ["EUR", "BRL", "CAD", "USD", "TRY", "JPY"]
+class Converter:
+    #Definindo as moedas disponíveis para conversão
+    def __init__(self):
+           self.availableCoins = ["EUR", "BRL", "TRY", "JPY", "CAD", "USD"]
 
-while True:
-    while True:
-        print("==-==-==-==-==-==-==-==-==-==-==-==-==\nINSIRA OS ACRÔNIMOS DA MOEDA VIGENTE:")
-        for coin in availableCoin: 
-            print(coin)
-        actualCoin = str(input("==-==-==-==-==-==-==-==-==-==-==-==-==\n")).upper()
+    #Pegando os valores
+    def actualCoin(self):
+        while True:
+            print("-="*18)
+            print("Opções disponíveis:\n", self.availableCoins)
+            print("-="*18)
+            actualCoin = str(input("\nINSIRA OS ACRÔNIMOS DA MOEDA:\n")).upper()
 
-        if actualCoin not in availableCoin:
-            print("Insira uma moeda válida!\n")
-        else:
-            break
+            if actualCoin not in self.availableCoins:
+                print("\n\nInsira um valor válido!\n\n")
+            else:
+                return actualCoin
 
-    while True:
-        value = float(input("INSIRA O VALOR QUE DESEJA CONVERTER:\n"))
+    def value(self):
+        while True:
+            value = float(input("INSIRA O VALOR QUE DESEJA CONVERTER\n"))
 
-        if value <= 0:
-            print("Insir um valor acima de zero")
-        else:
-            break
+            if value <= 0:
+                print("Insira um valor válido!")
+            else:
+                return value
+
+    def converterCoin(self):
+        while True:
+            print("-="*18)
+            print("Opções disponíveis:\n", self.availableCoins)
+            print("-="*18)
+
+            converterCoin = str(input("INSIRA OS ACRÔNIMOS DA MOEDA CONVERSORA:\n")).upper()
+
+            if converterCoin not in self.availableCoins:
+                print("\n\nInsira um valor válido!\n\n")
+            else:
+                return converterCoin
+
+    #Convertendo efetivamente
+    def converting(self, actualCoin, value, converterCoin):
+
+        url = "https://www.google.com/finance/quote/{}-{}".format(actualCoin, converterCoin)
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        price = soup.find('div', class_="YMlKec fxKbKc")
+
+        print("Valor convertido: ${:.2f} {}".format(value*(float(price.text)), converterCoin) )
+
+
+
+converter = Converter()
+
+actualCoin = converter.actualCoin()
+value = converter.value()
+converterCoin = converter.converterCoin()
+
+converter.converting(actualCoin, value, converterCoin)
+    
 
     
-    while True:
-        print("==-==-==-==-==-==-==-==-==-==-==-==-==\nINSIRA OS ACRÔNIMOS DA MOEDA CONVERSORA:")
-        for coin in availableCoin: 
-            print(coin)
-        secondaryCoin = str(input("==-==-==-==-==-==-==-==-==-==-==-==-==\n")).upper()
-
-        if secondaryCoin not in availableCoin:
-            print("Insira uma moeda válida!\n")
-        else:
-            break
-
-
-
-    url = "https://www.google.com/finance/quote/{}-{}".format(actualCoin, secondaryCoin)
-    page = requests.get(url)
-
-    soup = BeautifulSoup(page.content, "html.parser")
-    links = soup.find_all("div")
-    price = soup.find('div', class_="YMlKec fxKbKc")
-
-
-    print("Valor convertido: ${:.2f} {}".format(value*(float(price.text)), secondaryCoin.upper()))
